d49c25d8063da7f191ba7b017087ade9c594791e
Rian <rian.ace.2003@gmail.com>
2023-05-30 10:38:47 -0300

    ...

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const { config } = require('dotenv');
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(routes);
 app.use(express.json());
 
