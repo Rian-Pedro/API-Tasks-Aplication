6a900c2133bcaad1a69a7326c835d9f96937913f
Rian <rian.ace.2003@gmail.com>
2023-05-30 10:49:44 -0300

    ...

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const { config } = require('dotenv');
 
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
 });
 app.use(routes);
 app.use(express.json());
